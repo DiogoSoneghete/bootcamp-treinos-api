2a595e15f81dfd50bf6c1d874514b206e9522f18
Diogo Soneghete <diogosonegueti@gmail.com>
2026-03-11 20:59:14 -0300

    feat: add workout session table and cascade deletion to schema and chore: move db credentials to .env

---
+++ src/schemas/index.ts
@@ -0,0 +1,31 @@
+import z from "zod";
+
+import { Weekday } from "../generated/prisma/enums.js";
+
+export const ErrorSchema = z.object({
+  error: z.string(),
+  code: z.string(),
+});
+
+export const WorkoutPlanSchema = z.object({
+  id: z.uuid(),
+  name: z.string().trim().min(1),
+  isActive: z.boolean(),
+  workoutDays: z.array(
+    z.object({
+      name: z.string().trim().min(1),
+      weekday: z.enum(Weekday),
+      isRest: z.boolean(),
+      estimatedDurationInSeconds: z.number().min(1),
+      exercises: z.array(
+        z.object({
+          order: z.number().min(0),
+          name: z.string().trim().min(1),
+          sets: z.number().min(1),
+          reps: z.number().min(1),
+          restTimeSeconds: z.number().min(1),
+        }),
+      ),
+    }),
+  ),
+});
